bd3e28563586cb6894c1f8f202492d986887871b
DhruvJais01 <119504272+DhruvJais01@users.noreply.github.com>
2023-11-25 06:20:28 +0000

    Created main and FileDownloader

--- FileDownloader.java
+++ FileDownloader.java
@@ -1,28 +1,28 @@
 class FileDownloader implements Runnable {
-    private volatile boolean downloadCancelled = false;
+  private volatile boolean downloadCancelled = false;
 
-    public void cancelDownload() {
-        downloadCancelled = true;
-    }
+  public void cancelDownload() {
+    downloadCancelled = true;
+  }
 
-    @Override
-    public void run() {
-        try {
-            for (int i = 1; i <= 10; i++) {
-                if (downloadCancelled) {
-                    System.out.println("Download cancelled. Cleanup resources and exit.");
-                    return;
-                }
-
-                // Simulate downloading a portion of the file
-                System.out.println("Downloading part " + i);
-                TimeUnit.SECONDS.sleep(1);
-            }
-
-            System.out.println("Download complete.");
-        } catch (InterruptedException e) {
-            // Handling interruption and cleaning up (if necessary)
-            System.out.println("Thread has been interrupted. Cleanup resources and exit.");
+  @Override
+  public void run() {
+    try {
+      for (int i = 1; i <= 10; i++) {
+        if (downloadCancelled) {
+          System.out.println("Download cancelled. Cleanup resources and exit.");
+          return;
         }
+
+        // Simulate downloading a portion of the file
+        System.out.println("Downloading part " + i);
+        TimeUnit.SECONDS.sleep(1);
+      }
+
+      System.out.println("Download complete.");
+    } catch (InterruptedException e) {
+      // Handling interruption and cleaning up (if necessary)
+      System.out.println("Thread has been interrupted. Cleanup resources and exit.");
     }
+  }
 }
--- Main.java
+++ Main.java
@@ -1,19 +1,19 @@
 class Main {
   public static void main(String[] args) {
-      FileDownloader downloader = new FileDownloader();
-      Thread downloadThread = new Thread(downloader);
+    FileDownloader downloader = new FileDownloader();
+    Thread downloadThread = new Thread(downloader);
 
-      // Start the download thread
-      downloadThread.start();
+    // Start the download thread
+    downloadThread.start();
 
-      // Simulate user cancelling the download after 5 seconds
-      try {
-          TimeUnit.SECONDS.sleep(5);
-      } catch (InterruptedException e) {
-          e.printStackTrace();
-      }
+    // Simulate user cancelling the download after 5 seconds
+    try {
+      TimeUnit.SECONDS.sleep(5);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
 
-      // Interrupt the download thread to signal cancellation
-      downloadThread.interrupt();
+    // Interrupt the download thread to signal cancellation
+    downloadThread.interrupt();
   }
 }
